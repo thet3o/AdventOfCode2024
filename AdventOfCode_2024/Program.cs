bdb8f4606baaf79479acc7bc933c10b49d4420eb
Matteo Vacalebri <vacalebrimatteo@gmail.com>
2024-12-01 16:04:49 +0100

    First commit

---
+++ AdventOfCode_2024/Program.cs
@@ -0,0 +1,37 @@
+﻿// DAY ONE 1ST DECEMBER \\
+// part one
+
+var lines = File.ReadAllLines(@"../../../PuzzlesInput/Day01_A.txt");
+int[] leftList = new int[lines.Length];
+int[] rightList = new int[lines.Length];
+
+for (int i = 0; i < lines.Length; i++)
+{
+    var values = lines[i].Split("   ");
+    leftList[i] = int.Parse(values[0]);
+    rightList[i] = int.Parse(values[1]);
+}
+
+Array.Sort(leftList);
+Array.Sort(rightList);
+
+int sum = 0;
+
+for (int i = 0; i < leftList.Length; i++)
+{
+    sum += Math.Abs(leftList[i] - rightList[i]);
+}
+
+Console.WriteLine($"The sum of the left and right distances is: {sum}");
+
+
+// part two
+
+int similarityScore = 0;
+
+for (int i = 0; i < leftList.Length; i++)
+{
+    similarityScore += leftList[i] * rightList.Count((val => val == leftList[i]));
+}
+
+Console.WriteLine($"The similarity score is: {similarityScore}");
